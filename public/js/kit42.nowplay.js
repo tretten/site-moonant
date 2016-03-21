9cc691dce0f723763575ea8753848965954a47a7
Erlan <ebekenov@hungryfishmedia.com>
2016-03-21 16:40:37 -0400

    Load GA on each page

--- public/js/kit42.nowplay.js
+++ public/js/kit42.nowplay.js
@@ -14,6 +14,13 @@ function init_buttons(){
     load_js("likebtn_wjs", "//w.likebtn.com/js/w/widget.js");
     load_js("yastatic_es5", "//yastatic.net/es5-shims/0.0.2/es5-shims.min.js");
     load_js("yastatic_share", "//yastatic.net/share2/share.js");
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-73847830-1', 'auto');
+    ga('send', 'pageview');
 }
 
 window.onpopstate = function(e){
